76fa021745235c66a86cce53a488d4f7a8546fc1
Dylan Wright <wright.dylan5@gmail.com>
2019-05-23 10:01:22 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+
+var express = require("express");
+var mongojs = require("mongojs");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var exphb = require("express-handlebars");
+
+var app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+app.get("/all", function(req, res) {
+  db.scrapedData.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });
+});
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
+  var $ = cheerio.load(response.data);
+  var results = [];
+  $("p.title").each(function(i, element) {
+    var title = $(element).text();
+    var link = $(element).children().attr("href");
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+  console.log(results);
+});
+});
+
