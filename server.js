14880b2e2297fd0c8b922a73f8a625a8fdfe4929
Dylan Wright <wright.dylan5@gmail.com>
2019-05-30 15:56:14 -0400

    foundation

--- server.js
+++ server.js
@@ -7,38 +7,24 @@ var exphb = require("express-handlebars");
 
 var app = express();
 
-app.get("/", function(req, res) {
-  res.send("Hello world");
+var PORT = process.env.PORT || 3000;
+
+var routes = require("./routes");
+var db = require("./models");
+
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function() {
+  console.log("Listening on port: " + PORT);
 });
 
-app.get("/all", function(req, res) {
-  db.scrapedData.find({}, function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-    }
-  });
-});
-
-app.get("/scrape", function(req, res) {
-  axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
-  var $ = cheerio.load(response.data);
-  var results = [];
-  $("p.title").each(function(i, element) {
-    var title = $(element).text();
-    var link = $(element).children().attr("href");
-    results.push({
-      title: title,
-      link: link
-    });
-  });
-  console.log(results);
-  });
-  res.send("Scrape Complete");
-});
-
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
